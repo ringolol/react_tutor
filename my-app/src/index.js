db6abc44a2fc95722c3d90a32ca5d6c64947f81e
Ringolol <createsite108@gmail.com>
2020-12-07 03:42:39 +0300

    refactor and comment

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,20 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-// class Square extends React.Component {
-//     render() {
-//         return (
-//             <button 
-//                 className="square"
-//                 onClick={() => 
-//                     this.props.onClick()}
-//             >
-//                 {this.props.value}
-//             </button>
-//         );
-//     }
-// }
 
+/**
+ * Square of the tic-tac-toe board
+ */
 function Square(props) {
     return (
         <button 
@@ -27,6 +17,9 @@ function Square(props) {
     );
 }
 
+/**
+ * Board of the tic-tac-toe game
+ */
 class Board extends React.Component {
     renderSquare(i) {
         return (
@@ -40,77 +33,117 @@ class Board extends React.Component {
 
     render() {
         let elems = new Array(3).fill(null);
-        const renderCol = (
-            (row) => elems.map(
-                (_, col) => this.renderSquare(row*elems.length + col)
-            )
-        );
-        const renderRow = () => elems.map((_, row) => 
-            <div className="board-row">{renderCol(row)}</div>
+        // columns of the board
+        const renderCols = (row) => elems.map(
+            (_, col) => this.renderSquare(row*elems.length + col)
         );
 
+        // rows of the board
+        const renderRows = () => elems.map(
+            (_, row) => <div className="board-row">
+                {renderCols(row)}
+            </div>
+        );
+        
         return (
-            <div>{renderRow()}</div>
+            <div>{renderRows()}</div>
         );
     }
 }
 
+/**
+ * Tic-tac-toe engine
+ */
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            // history of game states
             history: [{
                 squares: Array(9).fill(null),
             }],
+            // the current step number
             stepNumber: 0,
+            // the next player is X
             xIsNext: true,
-            selectedButton: null,
+            // the selected history state
+            selectedHistory: null,
+            winner: null,
+            // the winning triplet
             winLine: null,
+            // shown direction of game states
+            movesAsc: true,
         }
     }
 
+    /**
+     * Handle click on the i-th square
+     * @param {*} i the index of the clicked square
+     */
     handleClick(i) {
+        // clone history and forget newer board states
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        // current game state
         const current = history[history.length - 1];
+        // current board state
         const squares = current.squares.slice();
+
+        // the game is over or the clicked square is already filled
         if(calculateWinner(squares) || squares[i]) {
             return;
         }
+
+        // fill the square
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        // update game state
         this.setState({
+            // append new game state
             history: history.concat([{
                 squares: squares,
                 col: i%3,
                 row: Math.floor(i/3),
             }]),
+            // toggle next player
             xIsNext: !this.state.xIsNext,
+            // upd history step and selected history
             stepNumber: history.length,
-            movesAsc: true,
-            selectedButton: history.length,
+            selectedHistory: history.length,
+        });
+
+        this.detWinner(squares);
+    }
+
+    /**
+     * Update winner info
+     * @param {Array} squares The tic-tac-toe board
+     */
+    detWinner(squares) {
+        const winData = calculateWinner(squares);
+        this.setState({
+            winner: winData?.winner,
+            winLine: winData?.winLine,
         });
     }
 
+    /**
+     * Load board state from step
+     * @param {*} step the number of step since the begining of the game
+     */
     jumpTo(step) {
         this.setState({
             stepNumber: step,
             xIsNext: (step % 2) === 0,
         });
+        this.detWinner(this.state.history[step].squares);
     }
 
-    render() {
-        let history = this.state.history.slice();
-        const current = history[this.state.stepNumber];
-        history = this.state.movesAsc ? history : history.reverse();
-        const winData = calculateWinner(current.squares);
-        let winner;
-        let winLine;
-        if (winData) {
-            winner = winData.winner;
-            winLine = winData.winLine;
-        }
-        this.state.winLine = winLine;
-        
-        const moves = history.map((step, move) => {
+    /**
+     * Draw the list of moves (game states)
+     * @param {Array} history array of game states
+     */
+    renderMoves(history) {
+        return history.map((step, move) => {
             move = this.state.movesAsc ? move : history.length - move - 1;
             const desc = move ?
                 `Go to move #${move} (${step.row}, ${step.col})`:
@@ -120,22 +153,32 @@ class Game extends React.Component {
                     <button onClick={() => {
                         this.jumpTo(move);
                         this.setState({
-                            selectedButton: move,
+                            selectedHistory: move,
                         });
                     }}>
-                        {this.state.selectedButton===move ? <b>{desc}</b> : desc }
+                        {this.state.selectedHistory===move ? <b>{desc}</b> : desc }
                     </button>
                 </li>
             );
         });
+    }
 
+    render() {
+        let history = this.state.history.slice();
+        // current game state
+        const current = history[this.state.stepNumber];
+        // reverse history if moovesAsc is set true
+        history = this.state.movesAsc ? history : history.reverse();
+
+        // game status
         let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
+        if (this.state.winner) {
+            status = 'Winner: ' + this.state.winner;
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
 
+        // render history list, status and game board
         return (
             <div className="game">
                 <div className="game-board">
@@ -154,20 +197,20 @@ class Game extends React.Component {
                     }}>
                         {this.state.movesAsc ? "^^^" : "vvv"}
                     </button>
-                    <ol reversed={!this.state.movesAsc}>{moves}</ol>
+                    <ol reversed={!this.state.movesAsc}>
+                        {this.renderMoves(history)}
+                    </ol>
                 </div>
             </div>
         );
     }
 }
 
-// ========================================
-
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
-
+/**
+ * Calculate the winner of a tic-tac-toe game
+ * @param {Array} squares an array representation of a tic-tac-toe board
+ * @return {Array|null} an array of the winner tag and the won triplet or null
+ */
 function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
@@ -196,3 +239,12 @@ function calculateWinner(squares) {
     }
     return null;
   }
+
+
+/**
+ * React entry point
+ */
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
